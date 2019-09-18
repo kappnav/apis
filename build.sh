adba22e102059f0bdb3387a707aec0901ca1be3b
Roger Cundiff <rogerleecundiff@gmail.com>
2019-09-18 17:13:42 -0500

    Remove unused ARCH from build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -Eeo pipefail
 
 IMAGE=kappnav-apis
 VERSION=0.1.1
-ARCH=amd64
 
-echo "Building ${IMAGE}"
+echo "Building ${IMAGE} ${VERSION}"
 docker build --build-arg VERSION=$VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t ${IMAGE} .
