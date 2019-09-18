ea7ec167432ae48926269ae0fe5e7aff6f8b3cf2
amylin1 <amylin@us.ibm.com>
2019-09-18 13:08:34 -0500

    Issue#9: Add new function to return list of pod for an app

--- src/main/java/application/rest/v1/actions/FunctionResolver.java
+++ src/main/java/application/rest/v1/actions/FunctionResolver.java
@@ -32,6 +32,7 @@ public final class FunctionResolver implements Resolver {
         functions = new HashMap<>();
         addFunction(new KubectlGetFunction());
         addFunction(new PodlistFunction());
+        addFunction(new AppPodlistFunction());
     }
     
     private static void addFunction(Function function) {
