b73030da5597489dab53ec99ff27eb7d4d5a4865
Roger Cundiff <rogerleecundiff@gmail.com>
2019-10-25 15:32:39 -0500

    version update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -Eeo pipefail
 
 IMAGE=kappnav-apis
-VERSION=0.1.1
+VERSION=0.1.2
 
 echo "Building ${IMAGE} ${VERSION}"
 docker build --build-arg VERSION=$VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t ${IMAGE} .
