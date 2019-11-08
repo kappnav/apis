b3ce9e51a0ecb0ae49f4f1be2401927a43105b55
Roger Cundiff <rogerleecundiff@gmail.com>
2019-11-08 08:15:53 -0600

    Add default version number for Travis builds

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -Eeo pipefail
 
-. ../build/version.sh
+# Travis builds won't have a peer build dir
+VERSION=x.x.x
+if [ -e ../build/version.sh ]; then
+    . ../build/version.sh
+fi
+
 IMAGE=kappnav-apis
 
 echo "Building ${IMAGE} ${VERSION}"
-docker build --pull --build-arg VERSION=$VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t ${IMAGE} .
+# docker build --pull --build-arg VERSION=$VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t ${IMAGE} .
