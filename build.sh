7b88c6d7d43a4fc998942b2a31a25325aa95abe7
Roger Cundiff <rogerleecundiff@gmail.com>
2020-05-26 15:19:47 -0500

    Push image on merge to master

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -Eeo pipefail
 
-# Travis builds won't have a peer build dir
-VERSION=x.x.x
-if [ -e ../build/version.sh ]; then
-    . ../build/version.sh
-fi
+. ../build/version.sh
 
 IMAGE=kappnav-apis
 
 echo "Building ${IMAGE} ${VERSION}"
 COMMIT=$(git rev-parse HEAD)
 docker build --pull --build-arg VERSION=$VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg COMMIT=$COMMIT -t ${IMAGE} .
+
+# If this build is being done on an update to the master branch then tag the image as "dev" and push to docker hub kappnav org
+if [ "$TRAVIS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH"  == "master" ]; then
+    echo $DOCKER_PWD | docker login docker.io -u $DOCKER_USER --password-stdin
+    targetImage=docker.io/kappnav/inv:dev
+    echo "Pushing Docker image $targetImage"
+    docker tag ${IMAGE} ${targetImage}
+    docker push ${targetImage}
+fi
